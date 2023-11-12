ed2f717e1d561ee51980c48c288aea3520ca602f
guicbelon <guilherme.c.belon@gmail.com>
2023-11-12 20:15:43 -0300

    feat: base login, logout and register

--- qualafestasite/settings.py
+++ qualafestasite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'qualafesta.apps.QualafestaConfig', # adicione esta linha
+    'qualafesta.apps.QualafestaConfig', 
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
--- qualafestasite/urls.py
+++ qualafestasite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path # modificar esta linha
+from django.urls import include, path 
 
 urlpatterns = [
-    path('qualafesta/', include('qualafesta.urls')), # adicionar esta linha
+    path('qualafesta/', include('qualafesta.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
