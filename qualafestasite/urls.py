39ec4cfe4eb1198622e77e307591b3bf1edddb36
Matheus <matheuslcavini@usp.br>
2023-11-14 23:47:11 -0300

    Create customer base template
    Criação do template básico do usuário cliente que será extendido por todas as páginas relativas a ele. Inclui a navbar responsiva, sem, ainda, as funções de navegar ao clicar

--- qualafestasite/urls.py
+++ qualafestasite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('qualafesta.urls')),
     path('admin/', admin.site.urls),
 ]
+
