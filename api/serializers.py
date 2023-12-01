00562a9adc7813a531eeccd5e646ed18b844b89b
Matheus <matheuslcavini@usp.br>
2023-11-30 21:30:47 -0300

    Include artists participation serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 
-from qualafesta.models import Event
+from qualafesta.models import Event, ArtistParticipation
+
+
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArtistParticipation
+        fields = ['id', 'artist_name', 'begin_time', 'end_time', 'artist_image']
 
 
 class EventSerializer(serializers.ModelSerializer):
+    artistparticipation_set = ArtistSerializer(many=True, read_only=True)
     class Meta:
         model = Event
-        fields = ['id', 'name', 'description', 'date_time', 'location']
+        fields = ['id', 'name', 'description', 'date_time', 'location', 'artistparticipation_set']
 
 
